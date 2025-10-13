4ae50b9947be22d0f940e2ab6a3f3e79a25ce548
Jerway <jzha0405@student.monash.edu>
2025-10-13 23:41:45 +1100

    added example bar chart

---
+++ Week 7 Studio/Part B - Intro to vega-lite/Test/vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "daily_costs.vg.json"
+vegaEmbed("#bar_chart", vg_1).then(function(result){
+    // Acess the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
